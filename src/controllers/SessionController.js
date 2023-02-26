5fbf9e41e4a88cdc858091b7caeda29077c7816a
Deivid Martins <md031194@gmail.com>
2023-02-26 01:53:21 -0300

    mvc

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,17 @@
+//metodos index, show, update, store, destroy
+
+/*index: listagem de sessoes
+store: Criar uma sessao
+show: quando queremos listas uma Unica sessao
+update: quando queremos alterar alguma sessao
+destroy: quando queremos deletar uma sessao
+
+*/
+
+
+class SessionController{
+
+
+}
+
+export default new SessionController();
